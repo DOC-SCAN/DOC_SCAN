4569f1c157980e040cc05b88d2cd5433f61cff56
DOC-SCAN <111710418+DOC-SCAN@users.noreply.github.com>
2022-08-25 02:03:16 +0500

    added the oracle ipd query

---
+++ oracle_apis.py
@@ -0,0 +1,25 @@
+import cx_Oracle
+import pandas as pd
+import json
+
+
+def ipd_patient_details():
+    dsn_tns = cx_Oracle.connect('asad_25510/asad#123@developdb.shifa.com.pk:1521/devdata.shifa.com.pk')
+    cursor = dsn_tns.cursor()
+
+    mr = "958005"
+    mr = "'" + mr + "'"
+    query = "SELECT cn.pc, cn.visit_id, a.fld_dat_adm_date, sp.speciality_name, d.doctor_id, d.consultant from " \
+            "ADMISSION.TBL_ADMISSION A, emr.const_notes         CN, doctors                 d,  specialities            " \
+            "sp  WHERE a.pk_str_admission_id = cn.id_ and a.fk_int_admitting_dr_id = d.doctor_id  and " \
+            "d.primary_speciality_id = sp.speciality_id  and a.mr# = " + mr
+
+    for row in cursor.execute(query):
+        df = pd.DataFrame(row, index=['Patient_Complain', 'Admission_ID', 'Admission_Data', 'Admitted_In_Speciality',
+                                      'Admitting_doctor_ID', 'Admitting_Doctor_Name'], )
+        # print(df)
+        print(pd.DataFrame.to_json(df, orient='columns'))
+
+
+if __name__ == '__main__':
+    ipd_patient_details()
