5bd39d3122a4c47dbf3a71b5f4a045b10f03369e
DOC-SCAN <111710418+DOC-SCAN@users.noreply.github.com>
2022-09-14 12:15:06 +0500

    ROLLBACK ROLLBACK ROLLBACK
    bcrypt

--- pipeline.py
+++ pipeline.py
@@ -15,7 +15,8 @@ import datetime
 from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from pymongo import MongoClient
-from flask_bcrypt import Bcrypt
+#from flask_bcrypt import Bcrypt
+import bcrypt as bc
 import clone_server
 import doc_id_from_mongo
 from PIL import Image
@@ -26,7 +27,7 @@ compress = Compress()
 app = Flask(__name__)
 cors = CORS(app)
 compress.init_app(app)
-bcrypt = Bcrypt(app)
+#bcrypt = Bcrypt(app)
 
 api = Api(app)
 jwt = JWTManager(app)
@@ -51,7 +52,7 @@ def login():
         print("in if")
         encrpted_password = login_details['PASSWORD'].encode("utf-8")
         print(user_from_db['PASSWORD'])
-        if bcrypt.check_password_hash(user_from_db['PASSWORD'].encode("utf-8"), encrpted_password):
+        if bc.checkpw(encrpted_password, user_from_db['PASSWORD'].encode("utf-8")):
             access_token = create_access_token(identity=user_from_db['USERNAME'])  # create jwt token
             return jsonify({"access_token": access_token,
                             "status": True
