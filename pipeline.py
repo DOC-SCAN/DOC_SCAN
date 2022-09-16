ff2ecc7577631249c13884f32dd4a3531dfc0e6d
DOC-SCAN <111710418+DOC-SCAN@users.noreply.github.com>
2022-09-16 10:49:48 +0500

    N|

--- pipeline.py
+++ pipeline.py
@@ -50,8 +50,9 @@ def login():
     if user_from_db:
         print("in if")
         encrpted_password = login_details['PASSWORD'].encode("utf-8")
+        print(encrpted_password)
         print(user_from_db['PASSWORD'])
-        if bc.checkpw(encrpted_password, user_from_db['PASSWORD']):#.encode("utf-8")):
+        if bc.checkpw("123".encode("utf-8"), "$2b$10$v1t3ouVjEdImQ9acJqtSHuUtxAgtd0Ttn/yFVtD.qSckU4iLbfr06".encode("utf-8")):
             access_token = create_access_token(identity=user_from_db['USERNAME'])  # create jwt token
             return jsonify({"access_token": access_token,
                             "status": True
