15ee61f11422838bc93ccf13973509ed204ad5b2
DOC-SCAN <111710418+DOC-SCAN@users.noreply.github.com>
2022-11-08 11:09:27 +0500

    ==PATCHES==

--- pipeline.py
+++ pipeline.py
@@ -26,7 +26,6 @@ import glob
 import os
 from weak_classifier import give_classes_data as gd
 from Mongo_APIS import get_by_mr as gmr
-from datetime import datetime as dt_tm
 
 compress = Compress()
 app = Flask(__name__)
@@ -92,7 +91,7 @@ def login_rolebase():
         if bc.checkpw(encrpted_password, user_from_db['PASSWORD'].encode("utf-8")):
             access_token = create_access_token(identity=user_from_db['USERNAME'])  # create jwt token
             doc_id.find_one_and_update({'USERNAME': str(login_details['USERNAME']).upper()},
-                                       {'$set', {'last_login', str(dt_tm.now())}})
+                                       {'$set', {'last_login', str(datetime.now())}})
             return jsonify({"access_token": access_token,
                             "is_admin": user_from_db['is_admin'],
                             "is_active": user_from_db['is_active'],
