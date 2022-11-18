2d041881ee611f8c0a2b5e9b4a1ef06a9c927677
DOC-SCAN <111710418+DOC-SCAN@users.noreply.github.com>
2022-11-18 11:54:13 +0500

    ==PATCHES==

--- jpg_compress_mechanisms.py
+++ jpg_compress_mechanisms.py
@@ -9,3 +9,8 @@ def resize_without_loosing_quality():
         foo = foo.resize(foo.size, Image.ANTIALIAS)
         foo.save(file, optimize=True, quality=30)
 
+
+def resize_on_the_go(path):
+    foo = Image.open(path)
+    foo = foo.resize((100, 127), Image.ANTIALIAS)
+    foo.save(path, optimize=True, quality=85)
