f7c9a0d1a1744b81e668dd4f0ad2c47373f23b6a
DOC-SCAN <111710418+DOC-SCAN@users.noreply.github.com>
2022-12-29 22:05:57 +0500

    Adding personal dash

--- pesonal_dash.py
+++ pesonal_dash.py
@@ -8,7 +8,15 @@ collection = db['DOCUMENTS']
 
 total_files = collection.distinct('mrno')
 total_documents = collection.count_documents({})
+total_unclassified = collection.count_documents({"class": "0"})
+current_classifier_accuracy = 100 - ((total_unclassified/total_documents)*100)
+total_classified = total_documents - total_unclassified
+total_space_used = db.command("")
 
 if __name__ == '__main__':
     print(len(total_files))
     print(total_documents)
+    print(total_unclassified)
+    print(round(current_classifier_accuracy, 2))
+    print(total_classified)
+
